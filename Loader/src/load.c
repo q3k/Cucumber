f2b233c1a551c08281a2f4fbf93686edb771ae32
Sergiusz Bazanski <sergiusz@q3k.org>
2011-08-25 01:53:43 +0200

    Loader: Fixed indentation/

--- Loader/src/load.c
+++ Loader/src/load.c
@@ -144,45 +144,45 @@ void print_hex(u64 Number)
 }
 
 struct elf_ident {
-	u32 Magic; // \x7FELF
-	u8 Class;
-	u8 Data;
-	u8 Version;
-	u8 Padding[9];
+    u32 Magic; // \x7FELF
+    u8 Class;
+    u8 Data;
+    u8 Version;
+    u8 Padding[9];
 };
 
 struct elf_header {
-	struct elf_ident Identification;
-	u16 Type;
-	u16 Machine;
-	u32 Version;
-	u64 Entry;
-	u64 ProgramHeaderOffset;
-	u64 SectionHeaderOffset;
-	u32 Flags;
-	u16 HeaderSize;
-	u16 ProgramHeaderEntrySize;
-	u16 NumProgramHeaderEntries;
-	u16 SectionHeaderEntrySize;
-	u16 NumSectionHeaderEntries;
-	u16 SectionEntryStrings;
+    struct elf_ident Identification;
+    u16 Type;
+    u16 Machine;
+    u32 Version;
+    u64 Entry;
+    u64 ProgramHeaderOffset;
+    u64 SectionHeaderOffset;
+    u32 Flags;
+    u16 HeaderSize;
+    u16 ProgramHeaderEntrySize;
+    u16 NumProgramHeaderEntries;
+    u16 SectionHeaderEntrySize;
+    u16 NumSectionHeaderEntries;
+    u16 SectionEntryStrings;
 };
 
 struct elf_section_header {
-	u32 Name;
-	u32 Type;
-	u64 Flags;
-	u64 Address;
-	u64 Offset;
-	u64 Size;
-	u32 Link;
-	u32 Info;
-	u64 Alignment;
-	u64 FixedSize;
+    u32 Name;
+    u32 Type;
+    u64 Flags;
+    u64 Address;
+    u64 Offset;
+    u64 Size;
+    u32 Link;
+    u32 Info;
+    u64 Alignment;
+    u64 FixedSize;
 };
 
 static inline void cpuid(u32 code, u32 *a, u32 *d) {
-	__asm__ volatile("cpuid":"=a"(*a),"=d"(*d):"0"(code):"ecx","ebx");
+    __asm__ volatile("cpuid":"=a"(*a),"=d"(*d):"0"(code):"ecx","ebx");
 }
 
 
@@ -207,84 +207,84 @@ u64 page_tab_high[512] __attribute__((aligned(0x1000)));
 
 u32 create_ia32e_paging(u64 KernelPhysicalStart, u64 KernelVirtualStart, u64 KernelSize)
 {
-	puts("Clearing paging structures...\n");
+    puts("Clearing paging structures...\n");
 
-	for (u16 i = 0; i < 512; i++)
-	{
-		pml4[i] = 0;
-		page_dir_ptr_tab_low[i] = 0;
-		page_dir_low[i] = 0;
-		page_tab_low[i] = 0;
-		page_dir_ptr_tab_high[i] = 0;
-		page_dir_high[i] = 0;
-		page_tab_high[i] = 0;
-	}
+    for (u16 i = 0; i < 512; i++)
+    {
+        pml4[i] = 0;
+        page_dir_ptr_tab_low[i] = 0;
+        page_dir_low[i] = 0;
+        page_tab_low[i] = 0;
+        page_dir_ptr_tab_high[i] = 0;
+        page_dir_high[i] = 0;
+        page_tab_high[i] = 0;
+    }
 
-	puts("Setting up identity paging for first 2MiB...\n");
-	pml4[GET_PML4_ENTRY(0)] = (u32)page_dir_ptr_tab_low | 3;
-	page_dir_ptr_tab_low[GET_PDP_ENTRY(0)] = (u32)page_dir_low | 3;
-	page_dir_low[GET_DIR_ENTRY(0)] = (u32)page_tab_low | 3;
+    puts("Setting up identity paging for first 2MiB...\n");
+    pml4[GET_PML4_ENTRY(0)] = (u32)page_dir_ptr_tab_low | 3;
+    page_dir_ptr_tab_low[GET_PDP_ENTRY(0)] = (u32)page_dir_low | 3;
+    page_dir_low[GET_DIR_ENTRY(0)] = (u32)page_tab_low | 3;
 
-	u64 Address = 0;
-	for (u16 i = 0; i < 512; i++)
-	{
-		page_tab_low[i] = Address | 3;
-		Address += 0x1000;
-	}
+    u64 Address = 0;
+    for (u16 i = 0; i < 512; i++)
+    {
+        page_tab_low[i] = Address | 3;
+        Address += 0x1000;
+    }
 
-	puts("Setting up paging for the kernel...\n");
-	u16 NumPages = KernelSize / 0x1000;
-	puts("  (0x");
-	print_hex(NumPages);
-	puts(" pages)\n");
+    puts("Setting up paging for the kernel...\n");
+    u16 NumPages = KernelSize / 0x1000;
+    puts("  (0x");
+    print_hex(NumPages);
+    puts(" pages)\n");
 
-	if (NumPages > 512)
-	{
-		puts("Error: Kernel size > 2MiB not implemented!");
-		return 1;
-	}
+    if (NumPages > 512)
+    {
+        puts("Error: Kernel size > 2MiB not implemented!");
+        return 1;
+    }
 
-	if (GET_PML4_ENTRY(KernelVirtualStart) != 0)
-	{
-		// We're NOT mapping the same PML4 entry as for identity mapping...
-		puts("Different PML4...\n");
-		pml4[GET_PML4_ENTRY(KernelVirtualStart)] = (u32)page_dir_ptr_tab_high | 3;
-		page_dir_ptr_tab_high[GET_PDP_ENTRY(KernelVirtualStart)] = (u32)page_dir_high | 3;
-		page_dir_high[GET_PDP_ENTRY(KernelVirtualStart)] = (u32)page_tab_high | 3;
-	}
-	else if (GET_PDP_ENTRY(KernelVirtualStart) != 0)
-	{
-		// We're NOT mapping the same page directory pointer table entry as for identity paging...
-		puts("Different PDPT... (");
-		print_hex(GET_PDP_ENTRY(KernelVirtualStart));
-		puts(")\n");
-		page_dir_ptr_tab_low[GET_PDP_ENTRY(KernelVirtualStart)] = (u32)page_dir_high | 3;
-		page_dir_high[GET_DIR_ENTRY(KernelVirtualStart)] = (u32)page_tab_high | 3;
-	}
-	else if (GET_DIR_ENTRY(KernelVirtualStart) != 0)
-	{
-		// We're NOT mapping the same page directory entry as for identity paging...
-		puts("Different DIR...\n");
-		page_dir_low[GET_DIR_ENTRY(KernelVirtualStart)] = (u32)page_tab_high | 3;
-	}
-	else
-	{
-		puts("Error: kernel overlaps 2MiB identity paging!\n");
-		return 1;
-	}
+    if (GET_PML4_ENTRY(KernelVirtualStart) != 0)
+    {
+        // We're NOT mapping the same PML4 entry as for identity mapping...
+        puts("Different PML4...\n");
+        pml4[GET_PML4_ENTRY(KernelVirtualStart)] = (u32)page_dir_ptr_tab_high | 3;
+        page_dir_ptr_tab_high[GET_PDP_ENTRY(KernelVirtualStart)] = (u32)page_dir_high | 3;
+        page_dir_high[GET_PDP_ENTRY(KernelVirtualStart)] = (u32)page_tab_high | 3;
+    }
+    else if (GET_PDP_ENTRY(KernelVirtualStart) != 0)
+    {
+        // We're NOT mapping the same page directory pointer table entry as for identity paging...
+        puts("Different PDPT... (");
+        print_hex(GET_PDP_ENTRY(KernelVirtualStart));
+        puts(")\n");
+        page_dir_ptr_tab_low[GET_PDP_ENTRY(KernelVirtualStart)] = (u32)page_dir_high | 3;
+        page_dir_high[GET_DIR_ENTRY(KernelVirtualStart)] = (u32)page_tab_high | 3;
+    }
+    else if (GET_DIR_ENTRY(KernelVirtualStart) != 0)
+    {
+        // We're NOT mapping the same page directory entry as for identity paging...
+        puts("Different DIR...\n");
+        page_dir_low[GET_DIR_ENTRY(KernelVirtualStart)] = (u32)page_tab_high | 3;
+    }
+    else
+    {
+        puts("Error: kernel overlaps 2MiB identity paging!\n");
+        return 1;
+    }
 
-	Address = KernelPhysicalStart;
-	for (u16 i = GET_TAB_ENTRY(KernelVirtualStart); i < GET_TAB_ENTRY(KernelVirtualStart) + NumPages; i++)
-	{
-		page_tab_high[i] = Address | 3;
-		/*print_hex(KernelVirtualStart + i * 0x1000);
-		puts(" -> ");
-		print_hex(Address);
-		puts("\n");*/
-		Address += 0x1000;
-	}
+    Address = KernelPhysicalStart;
+    for (u16 i = GET_TAB_ENTRY(KernelVirtualStart); i < GET_TAB_ENTRY(KernelVirtualStart) + NumPages; i++)
+    {
+        page_tab_high[i] = Address | 3;
+        /*print_hex(KernelVirtualStart + i * 0x1000);
+        puts(" -> ");
+        print_hex(Address);
+        puts("\n");*/
+        Address += 0x1000;
+    }
 
-	return 0;
+    return 0;
 }
 
 u32 g_multiboot_header;
@@ -299,8 +299,8 @@ u32 load(void *Multiboot, unsigned int Magic)
 
     if (Magic != 0x2BADB002)
     {
-	    puts("Error: not booted via Multiboot!\n");
- 	    return 0;
+        puts("Error: not booted via Multiboot!\n");
+        return 0;
     }
 
     u32 CPUID_A, CPUID_D;
@@ -310,8 +310,8 @@ u32 load(void *Multiboot, unsigned int Magic)
 
     if (!SupportFor64)
     {
-    	puts("Error: You CPU does not support long mode!\n");
-    	return 0;
+        puts("Error: You CPU does not support long mode!\n");
+        return 0;
     }
 
     u32 Flags = *((u32*)Multiboot);
@@ -320,9 +320,9 @@ u32 load(void *Multiboot, unsigned int Magic)
 
     if (!ModulesPresent)
     {
-	    puts("Error: no 64-bit kernel loaded!\n");
-	    puts("  (did you forget the module line in GRUB?)\n");
-	    return 0;
+        puts("Error: no 64-bit kernel loaded!\n");
+        puts("  (did you forget the module line in GRUB?)\n");
+        return 0;
     }
 
     u32 ModulesCount = *((u32*)Multiboot + 5);
@@ -336,7 +336,7 @@ u32 load(void *Multiboot, unsigned int Magic)
 
     if (ModulesCount != 1)
     {
-	    puts("Error: just one module is enough. Don't load a ton of them.\n");
+        puts("Error: just one module is enough. Don't load a ton of them.\n");
             return 0;
     }
 
@@ -347,20 +347,20 @@ u32 load(void *Multiboot, unsigned int Magic)
     struct elf_header *Header = *((struct elf_header **)ModulesAddress);
     if (Header->Identification.Magic != 0x464C457F)
     {
-	    puts("Error: Module is not an ELF file!\n");
-	    return 0;
+        puts("Error: Module is not an ELF file!\n");
+        return 0;
     }
 
     if (Header->Identification.Class != 2)
     {
- 	   puts("Error: Module is not a 64-bit ELF file!\n");
- 	   return 0;
+        puts("Error: Module is not a 64-bit ELF file!\n");
+        return 0;
     }
 
     if (!Header->Entry)
     {
- 	   puts("Error: Kernel does not have entry point!\n");
- 	   return 0;
+        puts("Error: Kernel does not have entry point!\n");
+        return 0;
     }
 
     puts("Entry point @");
@@ -369,8 +369,8 @@ u32 load(void *Multiboot, unsigned int Magic)
 
     if (Header->SectionHeaderEntrySize != sizeof(struct elf_section_header))
     {
-    	puts("Error: Weird section header entry size!\n");
-	    return 0;
+        puts("Error: Weird section header entry size!\n");
+        return 0;
     }
 
     struct elf_section_header *Sections = (struct elf_section_header *)((u32)Header + (u32)Header->SectionHeaderOffset);
@@ -389,35 +389,35 @@ u32 load(void *Multiboot, unsigned int Magic)
 
     for (u16 i = 0; i < Header->NumSectionHeaderEntries; i++)
     {
-	    s8* Name = (s8*)((u32)Header + (u32)StringSection->Offset + (u32)Sections[i].Name);
-	    u64 PhysicalAddress = (u32)Header + Sections[i].Offset;
-	    u64 VirtualAddress = Sections[i].Address;
+        s8* Name = (s8*)((u32)Header + (u32)StringSection->Offset + (u32)Sections[i].Name);
+        u64 PhysicalAddress = (u32)Header + Sections[i].Offset;
+        u64 VirtualAddress = Sections[i].Address;
 
-	    if (VirtualAddress)
-	    {
-	    	if (!StartVirtual)
-	    		StartVirtual = VirtualAddress;
+        if (VirtualAddress)
+        {
+            if (!StartVirtual)
+                StartVirtual = VirtualAddress;
 
-	    	if (!StartPhysical)
-	    		StartPhysical = PhysicalAddress;
+            if (!StartPhysical)
+                StartPhysical = PhysicalAddress;
 
-	    	if (ContinuityTest && VirtualAddress != ContinuityTest)
-			{
-				puts("Error: kernel is not continuous!\n");
-				return 0;
-			}
+            if (ContinuityTest && VirtualAddress != ContinuityTest)
+            {
+                puts("Error: kernel is not continuous!\n");
+                return 0;
+            }
 
-			ContinuityTest = VirtualAddress + Sections[i].Size;
-			Size += Sections[i].Size;
+            ContinuityTest = VirtualAddress + Sections[i].Size;
+            Size += Sections[i].Size;
 
-		    puts("-> Section ");
-		    puts(Name);
-		    puts(", 0x");
-		    print_hex(PhysicalAddress);
-		    puts(" will be located at 0x");
-		    print_hex(VirtualAddress);
-		    puts(".\n");
-	    }
+            puts("-> Section ");
+            puts(Name);
+            puts(", 0x");
+            print_hex(PhysicalAddress);
+            puts(" will be located at 0x");
+            print_hex(VirtualAddress);
+            puts(".\n");
+        }
     }
 
     puts("\nPaging setup:\n 0x");
@@ -429,7 +429,7 @@ u32 load(void *Multiboot, unsigned int Magic)
     puts(" bytes)\n");
 
     if (create_ia32e_paging(StartPhysical, StartVirtual, Size))
-    	return 0;
+        return 0;
     __asm__ volatile ("movl %cr4, %eax; bts $5, %eax; movl %eax, %cr4");
     __asm__ volatile ("movl %%eax, %%cr3" :: "a" (pml4));
     puts("CR3 is now pointing to PML4 (0x");
@@ -438,13 +438,13 @@ u32 load(void *Multiboot, unsigned int Magic)
 
     puts("Here it goes, enabling long mode...\n");
 
-    __asm__ volatile(	"movl $0xc0000080, %%ecx;\n"
-    					"rdmsr;\n"
-    					"orl $0x100, %%eax;\n"
-    					"wrmsr;\n"
-    					"movl %%cr0, %%ebx;\n"
-    					"bts $31, %%ebx;\n"
-    					"movl %%ebx, %%cr0;":::"eax","ebx","ecx");
+    __asm__ volatile(    "movl $0xc0000080, %%ecx;\n"
+                        "rdmsr;\n"
+                        "orl $0x100, %%eax;\n"
+                        "wrmsr;\n"
+                        "movl %%cr0, %%ebx;\n"
+                        "bts $31, %%ebx;\n"
+                        "movl %%ebx, %%cr0;":::"eax","ebx","ecx");
 
     puts("Now in 32-bit compability mode, jumping to the kernel...\n");
 

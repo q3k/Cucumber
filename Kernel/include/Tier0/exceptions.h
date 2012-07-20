d9d7114559cabc788a8f58e896f21cf6f521f3e6
Sergiusz Bazański <q3k@q3k.org>
2012-07-20 23:33:38 +0200

    redacted

--- Kernel/include/Tier0/exceptions.h
+++ Kernel/include/Tier0/exceptions.h
@@ -6,6 +6,8 @@
 
 void exceptions_init_simple(void);
 void exceptions_page_fault_isr(T_ISR_REGISTERS_ERR Registers);
+void exceptions_general_protection_isr(T_ISR_REGISTERS_ERR Registers);
 void exceptions_division_by_zero_isr(T_ISR_REGISTERS Registers);
+void exceptions_floating_point_isr(T_ISR_REGISTERS Registers);
 
 #endif

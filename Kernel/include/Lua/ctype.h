5e3c5566d93d7ae84fc2bc498d5d4268152034dd
Sergiusz Bazański <q3k@q3k.org>
2012-08-23 15:42:57 +0200

    Lua now compiles.
    But it still won't link against the kernel, as it's missing
    a lot of ANSI C functions. Time to find some BSD/MIT libc
    implementations...

---
+++ Kernel/include/Lua/ctype.h
@@ -0,0 +1,25 @@
+#ifndef __TIER0_ANSI_CTYPE_H__
+#define __TIER0_ANSI_CTYPE_H__
+
+int isalnum(int);
+int isalpha(int);
+
+int isascii(int);
+
+int isblank(int);
+int iscntrl(int);
+int isdigit(int);
+int isgraph(int);
+int islower(int);
+int isprint(int);
+int ispunct(int);
+int isspace(int);
+int isupper(int);
+int isxdigit(int);
+
+int toascii(int);
+
+int tolower(int);
+int toupper(int);
+
+#endif

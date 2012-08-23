f4ffd6d044db7e5af8b1be961490b7b88c2a3095
Sergiusz Bazański <q3k@q3k.org>
2012-08-23 15:44:36 +0200

    Including stubs for Lua to use.
    Because I forgot them.

--- Kernel/include/Lua/stdlib.h
+++ Kernel/include/Lua/stdlib.h
@@ -2,8 +2,14 @@
 #define __TIER0_ANSI_STDLIB_H__
 
 void *malloc(unsigned long long int size);
+void free(void *);
+void *realloc(void *ptr, unsigned long long int size);
 int abs(int X);
 void abort(void);
 long int strtol(const char *nptr, char **endptr, int base);
+void exit(int status);
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
 
 #endif
--- Kernel/include/Lua/string.h
+++ Kernel/include/Lua/string.h
@@ -7,31 +7,13 @@
 const char *strchr (const char *str, int character);
 inline int strcmp(const char *s1, const char *s2);
 char *strpbrk(const char *s1, const char *s2);
+char *strerror(int errnum);
+char *strstr(const char *haystack, const char *needle);
+unsigned long long int strspn(const char *s, const char *accept);
+void *memchr(const void *s, int c, unsigned long long int n);
+char *strcpy(char *dest, const char *src);
 
-const char *strchr (const char *str, int character)
-{
-    while (*str != 0)
-    {
-        if (*str == character)
-            return str;
-        str++;
-    }
-    return 0;
-}
-inline int strcmp(const char *s1, const char *s2)
-{
-    while((*s1 && *s2) && (*s1++ == *s2++));
-    return *(--s1) - *(--s2);
-}
-
-char *strpbrk(const char *s1, const char *s2)
-{
-    while(*s1)
-        if(strchr(s2, *s1++))
-            return (char*)--s1;
-    return 0;
-}
-
+#define strcoll(A, B) strcmp(A, B)
 #define strlen(A) kstrlen(A)
 #define memcpy(A, B, C) kmemcpy(A, B, C)
 #define memcmp(A, B, C) kmemcmp((const u8*)A, (const u8*)B, C)

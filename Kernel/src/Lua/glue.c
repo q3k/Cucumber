57601cac2ca17fbe7d906a61356bd142be99bbcf
Sergiusz Bazański <q3k@q3k.org>
2012-09-24 14:33:04 +0200

    Fixed Lua interpreter crap.
    My memcmp() implementation was non standards compliant. Fuck.

--- Kernel/src/Lua/glue.c
+++ Kernel/src/Lua/glue.c
@@ -259,6 +259,20 @@ void free(void *ptr)
 }
 
 // string.h implementation
+int memcmp(const void *s1, const void *s2, unsigned long int n)
+{
+    const unsigned char *p1 = s1, *p2 = s2;
+    while(n--)
+        if( *p1 != *p2 )
+            return *p1 - *p2;
+        else
+        {
+            p1++;
+            p2++;
+        }
+    return 0;
+}
+
 const char *strchr (const char *str, int character)
 {
     while (*str != 0)
